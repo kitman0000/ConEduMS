de5129f3b59ada6fe127f3d331607a2205861808
kitman0000 <2272068516@qq.com>
2018-12-10 13:52:16 +0800

    alpha 1.0 upload
    The first alpha vision of the website!

--- js/System.js
+++ js/System.js
@@ -1,13 +1,14 @@
 function getNavbar()
 {
 	result = "";
+	result += "<li id = AdministratorManage role=\"presentation\"><a href=\"AdministratorManage.html\">管理员管理</a></li>";
 	result += "<li id = MajorManage role=\"presentation\"><a href=\"MajorManage.html\">专业管理</a></li>";
-	result += "<li id = SchoolManage role=\"presentation\"><a href=\"SchoolManage.html\">教学点管理</a></li>";
+	result += "<li id = TrainingSchoolManage role=\"presentation\"><a href=\"TrainingSchoolManage.html\">教学点基本信息管理</a></li>";
 	result += "<li id = CourseManage role=\"presentation\"><a href=\"CourseManage.html\">课程管理</a></li>";
 	result += "<li id = TeacherManage role=\"presentation\"><a href=\"TeacherManage.html\">教师管理</a></li>";
 	result += "<li id = ClassManage role=\"presentation\"><a href=\"ClassManage.html\">教学班管理</a></li>";
 	result += "<li id = StudentManage role=\"presentation\"><a href=\"StudentManage.html\">学生管理</a></li>";
 	result += "<li id = ScoreManage role=\"presentation\"><a href=\"ScoreManage.html\">成绩管理</a></li>";
-	result += "<li id = StudentSreach role=\"presentation\"><a href=\"StudentSreach.html\">学生查询</a></li>";
+	
 	return result;
 }
